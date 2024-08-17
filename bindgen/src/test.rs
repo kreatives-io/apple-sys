79c8277de1b13da6713e0c0f9115c6b7c3dec100
EduSantosBrito <edu.santos.brito@gmail.com>
2024-08-17 02:52:03 +0200

    chore: upgrade bindgen

--- bindgen/src/test.rs
+++ bindgen/src/test.rs
@@ -7,3 +7,17 @@ fn generate_core_foundation() {
         .generate()
         .expect("generate failed");
 }
+#[test]
+fn generate_core_media() {
+    Builder::with_builtin_config("CoreMedia", "macosx")
+        .expect("macosx sdk not found")
+        .generate()
+        .expect("generate failed");
+}
+#[test]
+fn generate_core_media_with_wrong_target() {
+    Builder::with_builtin_config("CoreMedia", "ios")
+        .expect("macosx sdk not found")
+        .generate()
+        .expect("generate failed");
+}
